690fed12d8bc8e920d71082d20c968fd5bda72e6
leonardo23hm <leonardo23hm@gmail.com>
2026-05-20 23:57:17 -0300

    Atualização do Schema

--- database/schema.sql
+++ database/schema.sql
@@ -7,7 +7,7 @@ CREATE DATABASE IF NOT EXISTS db_lgpd4devs
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
-USE db_lgpd4devs;
+USE db_lgpd4devs
 
 -- ============================================================
 -- TABELAS
